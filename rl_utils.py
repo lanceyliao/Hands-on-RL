aeb782d6c1826bfaf1de4ed7889c7104f723bd06
lanceyliao <lanceyliao@gmail.com>
2024-08-02 10:00:20 +0800

    10

--- rl_utils.py
+++ rl_utils.py
@@ -50,37 +50,78 @@ def moving_average(a, window_size):
     end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
     return np.concatenate((begin, middle, end))
 
+# def train_on_policy_agent(env, agent, num_episodes):
+#     return_list = []
+#     for i in range(10):
+#         with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
+#             for i_episode in range(int(num_episodes/10)):
+#                 episode_return = 0
+#                 transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
+#                 state = env.reset()
+#                 if len(state)!=2*2:
+#                     state = state[0]
+#                 done = False
+#                 ## 采样一条序列的
+#                 while not done:
+#                     action = agent.take_action(state)    ##  根据状态采取动作的
+#                     ##  环境执行动作，并反馈下一个状态、动作的奖励、是否完成、步长太长的，info
+#                     next_state, reward, terminated, truncated, info = env.step(action)
+#                     done = terminated | truncated       ## 终止或者步长太长，都会导致已经结束
+#                     ## record该序列的 该时刻状态、该时刻动作、下一个状态、动作的奖励、是否完成的
+#                     transition_dict['states'].append(state)
+#                     transition_dict['actions'].append(action)
+#                     transition_dict['next_states'].append(next_state)
+#                     transition_dict['rewards'].append(reward)
+#                     transition_dict['dones'].append(done)
+#                     state = next_state    ## 下一个状态赋值到当前状态
+#                     episode_return += reward  ##累加奖励的
+#                 return_list.append(episode_return)  ## 训练策略网络的，用一条序列来训练
+#                 agent.update(transition_dict)
+#                 if (i_episode+1) % 10 == 0:
+#                     pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
+#                 pbar.update(1)
+#     return return_list
+
 def train_on_policy_agent(env, agent, num_episodes):
     return_list = []
+    allimage = []
     for i in range(10):
-        with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
+        with tqdm(total=int(num_episodes / 10), desc=f'Iteration {i}') as pbar:
             for i_episode in range(int(num_episodes/10)):
                 episode_return = 0
-                transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
-                state = env.reset()
-                if len(state)!=2*2:
-                    state = state[0]
+                transition_dict = {'S': [], 'A': [], 'S_': [], 'R': [], 'dones': []}
+                s = env.reset()
+                if len(s) != 2 * 2:
+                    s = s[0]
                 done = False
                 ## 采样一条序列的
                 while not done:
-                    action = agent.take_action(state)    ##  根据状态采取动作的
+                    if (i_episode + 1) % 10 == 0 and i in [9]:
+                        img = env.render()
+                        allimage.append(img)
+                    # cv2.imshow("CartPole-v1", img)
+                    # cv2.waitKey(-1)
+                    a = agent.take_action(s)  ##  根据状态采取动作的
                     ##  环境执行动作，并反馈下一个状态、动作的奖励、是否完成、步长太长的，info
-                    next_state, reward, terminated, truncated, info = env.step(action)
-                    done = terminated | truncated       ## 终止或者步长太长，都会导致已经结束
+                    s_, r, terminated, truncated, info = env.step(a)
+                    done = terminated | truncated  ## 终止或者步长太长，都会导致已经结束
                     ## record该序列的 该时刻状态、该时刻动作、下一个状态、动作的奖励、是否完成的
-                    transition_dict['states'].append(state)
-                    transition_dict['actions'].append(action)
-                    transition_dict['next_states'].append(next_state)
-                    transition_dict['rewards'].append(reward)
+                    transition_dict['S'].append(s)
+                    transition_dict['A'].append(a)
+                    transition_dict['S_'].append(s_)
+                    transition_dict['R'].append(r)
                     transition_dict['dones'].append(done)
-                    state = next_state    ## 下一个状态赋值到当前状态
-                    episode_return += reward  ##累加奖励的
-                return_list.append(episode_return)  ## 训练策略网络的，用一条序列来训练
-                agent.update(transition_dict)
-                if (i_episode+1) % 10 == 0:
-                    pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
+                    s = s_  ## 下一个状态赋值到当前状态
+                    episode_return += r  ##累加奖励的
+                return_list.append(episode_return)
+                agent.update(transition_dict)  ## 训练策略网络的，用一条序列来训练
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({
+                        'episode': f'{num_episodes / 10 * i + i_episode + 1}',
+                        'return': f'{np.mean(return_list[-10:]):.3f}'
+                    })
                 pbar.update(1)
-    return return_list
+    return return_list, allimage
 
 def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
     return_list = []
