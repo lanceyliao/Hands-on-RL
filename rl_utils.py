94e3052fbefe1700ab487bb90cdb30fd9082d171
lanceyliao <lanceyliao@gmail.com>
2024-08-06 20:03:41 +0800

    12

--- rl_utils.py
+++ rl_utils.py
@@ -190,9 +190,9 @@ def compute_advantage(gamma, lmbda, td_delta):
     td_delta = td_delta.detach().numpy()
     advantage_list = []
     advantage = 0.0
-    for delta in td_delta[::-1]:
+    for delta in td_delta[::-1]:  # 从后往前逆序算，刚好可以算出广义优势估计的值
         advantage = gamma * lmbda * advantage + delta
-        advantage_list.append(advantage)
+        advantage_list.append(advantage)  # 保存每一步的优势估计
     advantage_list.reverse()
     return torch.tensor(advantage_list, dtype=torch.float)
 
